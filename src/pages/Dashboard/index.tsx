23972c6e8619ab3fb06d7dbd238186d92228c1ae
kkomyoung <kya754@gmail.com>
2026-01-29 14:39:01 +0900

    feat: 최근 건강검진 결과 화면 추가

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCheckupData } from '@/hooks/useCheckupData';
+import { SearchIcon } from 'lucide-react';
 
 export default function Dashboard() {
 	const navigate = useNavigate();
-	const { data, hasData } = useCheckupData();
+	const { data, hasData, clearData } = useCheckupData();
 
 	// 데이터가 없으면 홈으로 리다이렉트
 	useEffect(() => {
@@ -17,11 +18,63 @@ export default function Dashboard() {
 		return null;
 	}
 
-	return (
-		<div className="max-w-4xl mx-auto">
-			<h1 className="text-2xl font-bold mb-6">건강검진 결과</h1>
+	// 환자 이름
+	const patientName = data.patientName;
+	// 가장 최근 건강검진
+	const latestCheckup = data.overviewList?.length
+		? [...data.overviewList].sort((a, b) => b.checkupDate.localeCompare(a.checkupDate))[0]
+		: null;
 
-			<pre className="bg-gray-100 p-4 rounded-lg overflow-auto text-sm">{JSON.stringify(data, null, 2)}</pre>
+	// 건강검진 데이터가 없을 경우
+	if (!latestCheckup) {
+		return (
+			<div className="max-w-7xl mx-auto text-center mt-20">
+				<p className="text-2xl mb-4">
+					<strong className="text-3xl">{patientName}님,</strong> 최근 건강검진 결과가 없습니다.
+				</p>
+
+				<button
+					className="mx-auto mt-8 shadow-lg flex items-center justify-center bg-rose-500 text-white px-10 py-4 rounded-lg gap-2 text-xl font-semibold cursor-pointer"
+					type="button"
+					onClick={clearData}
+				>
+					<SearchIcon className="w-6" />
+					다른 사용자로 재조회하기
+				</button>
+			</div>
+		);
+	}
+
+	return (
+		<div className="max-w-7xl mx-auto">
+			<h1 className="text-3xl font-bold mb-6">{patientName}님의 최근 건강검진 결과입니다</h1>
+			<p className="text-lg font-semibold mb-4">검진일: {latestCheckup.checkupDate}</p>
+			<ul className="grid grid-cols-4 gap-4 mb-10">
+				<li className="bg-white border rounded-xl shadow-md p-6">
+					<dl>
+						<dt className="text-base">키</dt>
+						<dd className="text-2xl text-right">{latestCheckup.height}cm</dd>
+					</dl>
+				</li>
+				<li className="bg-white border rounded-xl shadow-md p-6">
+					<dl>
+						<dt className="text-base">몸무게</dt>
+						<dd className="text-2xl text-right">{latestCheckup.weight}kg</dd>
+					</dl>
+				</li>
+				<li className="bg-white border rounded-xl shadow-md p-6">
+					<dl>
+						<dt className="text-base">혈압</dt>
+						<dd className="text-2xl text-right">{latestCheckup.bloodPressure}mmHg</dd>
+					</dl>
+				</li>
+				<li className="bg-white border rounded-xl shadow-md p-6">
+					<dl>
+						<dt className="text-base">식전혈당</dt>
+						<dd className="text-2xl text-right">{latestCheckup.fastingBloodGlucose}mg/dL</dd>
+					</dl>
+				</li>
+			</ul>
 		</div>
 	);
 }
